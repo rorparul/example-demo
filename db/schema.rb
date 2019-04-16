7cc4885e10633fb9a32009a9bfe1b2d79bda4450
Parul <ror.parul@gmail.com>
2019-04-16 09:23:32 +0530

    added database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_18_025052) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
